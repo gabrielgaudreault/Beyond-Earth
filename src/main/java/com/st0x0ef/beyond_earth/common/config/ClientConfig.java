0e67d9d79c1a07d04484fbc84f322fb475ccef85
gabrielgaudreault <gabriel.gaudreault3@gmail.com>
2023-12-18 11:26:08 -0500

    fix crash in config + fix entity renderer registry

--- src/main/java/com/st0x0ef/beyond_earth/common/config/ClientConfig.java
+++ src/main/java/com/st0x0ef/beyond_earth/common/config/ClientConfig.java
@@ -11,19 +11,16 @@ public class ClientConfig {
     public static final ForgeConfigSpec.ConfigValue<Integer> PLANETS_FAST_STARS_COUNT;
 
     static {
-        BUILDER.push("Beyond Earth Client Config");
-
+        BUILDER.push("Orbit Sky");
             ORBIT_FANCY_STARS_COUNT = BUILDER.comment("How much fancy stars should appear in an orbit sky.").define("orbitFancyStarsCount", 13000);
             ORBIT_FAST_STARS_COUNT = BUILDER.comment("How much fast stars should appear in an orbit sky.").define("orbitFastStarsCount", 6000);
-            BUILDER.pop();
+        BUILDER.pop();
 
         BUILDER.push("Planet Sky");
             PLANETS_FANCY_STARS_COUNT = BUILDER.comment("How much fancy stars should appear in a planet sky.").define("planetFancyStarsCount", 13000);
             PLANETS_FAST_STARS_COUNT = BUILDER.comment("How much fast stars should appear in an planet sky.").define("planetFastStarsCount", 6000);
-            BUILDER.pop();
         BUILDER.pop();
 
         SPEC = BUILDER.build();
-        /** Entities */
     }
 }
