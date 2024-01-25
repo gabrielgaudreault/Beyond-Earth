a459efd76fde3b4e28edd6aaf32a68d4a80aa575
gabrielgaudreault <gabriel.gaudreault3@gmail.com>
2024-01-25 13:12:19 -0500

    fix hydrogen capability

--- src/main/java/com/st0x0ef/beyond_earth/common/registries/CapabilityRegistry.java
+++ src/main/java/com/st0x0ef/beyond_earth/common/registries/CapabilityRegistry.java
@@ -1,5 +1,6 @@
 package com.st0x0ef.beyond_earth.common.registries;
 
+import com.st0x0ef.beyond_earth.common.capabilities.hydrogen.HydrogenProvider;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.common.capabilities.CapabilityToken;
@@ -12,7 +13,6 @@ import com.st0x0ef.beyond_earth.common.capabilities.oxygen.OxygenProvider;
 
 @Mod.EventBusSubscriber(modid = BeyondEarth.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CapabilityRegistry {
-
     public static final Capability<ChunkOxygen> CHUNK_OXYGEN = CapabilityManager.get(new CapabilityToken<>() {
     });
     
@@ -20,5 +20,6 @@ public class CapabilityRegistry {
     public static void register(RegisterCapabilitiesEvent event) {
         event.register(OxygenProvider.class);
         event.register(ChunkOxygen.class);
+        event.register(HydrogenProvider.class);
     }
 }
